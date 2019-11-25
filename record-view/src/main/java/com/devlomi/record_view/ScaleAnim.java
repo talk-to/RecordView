6dd7f832fda6e12a5991a62018b4f3d995c5ab95
abhash.ya <2015csb1001@iitrpr.ac.in>
2019-11-25 11:19:49 +0530

    No scaling of mic button

--- record-view/src/main/java/com/devlomi/record_view/ScaleAnim.java
+++ record-view/src/main/java/com/devlomi/record_view/ScaleAnim.java
@@ -18,30 +18,11 @@ public class ScaleAnim {
 
     void start() {
         AnimatorSet set = new AnimatorSet();
-        ObjectAnimator scaleY = ObjectAnimator.ofFloat(view, "scaleY", 2.0f);
-
-        ObjectAnimator scaleX = ObjectAnimator.ofFloat(view, "scaleX", 2.0f);
-        set.setDuration(150);
-        set.setInterpolator(new AccelerateDecelerateInterpolator());
-        set.playTogether(scaleY, scaleX);
         set.start();
     }
 
     void stop() {
         AnimatorSet set = new AnimatorSet();
-        ObjectAnimator scaleY = ObjectAnimator.ofFloat(view, "scaleY", 1.0f);
-//        scaleY.setDuration(250);
-//        scaleY.setInterpolator(new DecelerateInterpolator());
-
-
-        ObjectAnimator scaleX = ObjectAnimator.ofFloat(view, "scaleX", 1.0f);
-//        scaleX.setDuration(250);
-//        scaleX.setInterpolator(new DecelerateInterpolator());
-
-
-        set.setDuration(150);
-        set.setInterpolator(new AccelerateDecelerateInterpolator());
-        set.playTogether(scaleY, scaleX);
         set.start();
     }
 }
